0ee93ea2658fe73386536dfa0b25d09a5fce3ee8
Jayjeet Chakraborty <jc.github@rediffmail.com>
2019-01-23 00:38:22 +0530

    Removed redundant code , toast , ui

--- app/src/main/java/com/pyp2p/jayjeet/pyp2pandroid/MainActivity.java
+++ app/src/main/java/com/pyp2p/jayjeet/pyp2pandroid/MainActivity.java
@@ -1,16 +1,10 @@
 package com.pyp2p.jayjeet.pyp2pandroid;
 
-import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -27,8 +21,8 @@ import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private static final int PERMISSION_REQUEST_CODE = 1;
-    private static final int QR_CODE_SCANNER_REQUEST_CODE = 2;
+    private static final int QR_CODE_SCANNER_REQUEST_CODE = 1;
+    private static final int FILE_BROWSER_REQUEST_CODE = 2;
 
     String serverHost;
     Integer serverPort;
@@ -47,66 +41,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         filePicker.setOnClickListener(this);
         scanQrCode.setOnClickListener(this);
-
-        if (Build.VERSION.SDK_INT >= 23) {
-            if (checkPermission()) {
-                Log.d("Permission", "Granted");
-            } else {
-                requestPermission();
-                Log.d("Permission", "Asking permission");
-            }
-        } else {
-
-        }
-
     }
 
-    private boolean checkPermission() {
-        int result = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE);
-        if (result == PackageManager.PERMISSION_GRANTED) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-
-    private void requestPermission() {
-
-        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
-            Toast.makeText(MainActivity.this, "Write External Storage permission allows us to do store images. Please allow this permission in App Settings.", Toast.LENGTH_LONG).show();
-        } else {
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSION_REQUEST_CODE:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Log.e("value", "Permission Granted, Now you can use local drive .");
-                } else {
-                    Log.e("value", "Permission Denied, You cannot use local drive .");
-                }
-                break;
-        }
-    }
 
     // Open the file browser intent
     public void performFileSearch() {
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         intent.setType("*/*");
-        startActivityForResult(intent, PERMISSION_REQUEST_CODE);
+        startActivityForResult(intent, FILE_BROWSER_REQUEST_CODE);
     }
+
 
     // This function is called after getting result from activity
     @Override
     public void onActivityResult(int requestCode, int resultCode,
                                  Intent resultData) {
 
-        if (requestCode == PERMISSION_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+        if (requestCode == FILE_BROWSER_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
             if (resultData != null) {
                 Uri uri = resultData.getData();
                 filePath = uri.getPath().substring(10);
@@ -117,13 +69,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 String[] addr = resultData.getData().toString().split(":");
                 serverHost = addr[0];
                 serverPort = Integer.parseInt(addr[1]);
-                if(filePath != null) {
+                if (filePath != null) {
                     Thread cThread = new Thread(new ServerThread());
                     cThread.start();
-                }
-                else
-                {
-                    Log.d("ERROR","No file picked");
+                } else {
+                    Toast.makeText(this, "No File Picked !", Toast.LENGTH_LONG).show();
                 }
             }
         }
