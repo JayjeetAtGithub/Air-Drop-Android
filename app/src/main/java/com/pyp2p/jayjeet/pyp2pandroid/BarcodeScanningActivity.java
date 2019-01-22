5ef4c8afbc447287fde4a7cc7df39cc4f6f5a3b9
Jayjeet Chakraborty <jc.github@rediffmail.com>
2019-01-22 18:41:41 +0530

    Added QR Scode scanning Facility

---
+++ app/src/main/java/com/pyp2p/jayjeet/pyp2pandroid/BarcodeScanningActivity.java
@@ -0,0 +1,46 @@
+package com.pyp2p.jayjeet.pyp2pandroid;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class BarcodeScanningActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    ZXingScannerView scannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        scannerView = new ZXingScannerView(this);
+        setContentView(scannerView);
+    }
+
+
+    @Override
+    public void handleResult(Result result) {
+        Intent data = new Intent();
+        data.setData(Uri.parse(result.getText()));
+        setResult(Activity.RESULT_OK, data);
+        finish();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        scannerView.stopCamera();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        scannerView.setResultHandler(this);
+        scannerView.startCamera();
+    }
+}
--- app/src/main/java/com/pyp2p/jayjeet/pyp2pandroid/MainActivity.java
+++ app/src/main/java/com/pyp2p/jayjeet/pyp2pandroid/MainActivity.java
@@ -2,75 +2,60 @@ package com.pyp2p.jayjeet.pyp2pandroid;
 
 import android.Manifest;
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
-import android.os.Environment;
-import android.support.annotation.IntRange;
-import android.support.annotation.IntegerRes;
+import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.InetAddress;
 import java.net.Socket;
-import java.nio.Buffer;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final int PERMISSION_REQUEST_CODE = 1;
-    String filePath = null;
-    Button connectButton,filePicker;
-    EditText host;
-    EditText port;
-    TextView fileUrl;
-    String serverIpAddress;
+    private static final int QR_CODE_SCANNER_REQUEST_CODE = 2;
+
+    String serverHost;
     Integer serverPort;
+    String filePath = null;
+    Button filePicker, scanQrCode;
+    TextView fileUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        connectButton = findViewById(R.id.connect_button);
+
         filePicker = findViewById(R.id.file_picker);
-        host = findViewById(R.id.host);
-        port = findViewById(R.id.port);
+        scanQrCode = findViewById(R.id.scan_code);
         fileUrl = findViewById(R.id.file_url);
-        connectButton.setOnClickListener(this);
+
         filePicker.setOnClickListener(this);
-        if (Build.VERSION.SDK_INT >= 23)
-        {
-            if (checkPermission())
-            {
-                Log.d("Permission","Granted");
+        scanQrCode.setOnClickListener(this);
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkPermission()) {
+                Log.d("Permission", "Granted");
             } else {
                 requestPermission();
-                Log.d("Permission","Asking permission");
+                Log.d("Permission", "Asking permission");
             }
-        }
-        else
-        {
+        } else {
 
         }
 
@@ -84,7 +69,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             return false;
         }
     }
-
 
 
     private void requestPermission() {
@@ -109,7 +93,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-
+    // Open the file browser intent
     public void performFileSearch() {
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
@@ -117,58 +101,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         startActivityForResult(intent, PERMISSION_REQUEST_CODE);
     }
 
+    // This function is called after getting result from activity
     @Override
     public void onActivityResult(int requestCode, int resultCode,
                                  Intent resultData) {
 
         if (requestCode == PERMISSION_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            Uri uri = null;
             if (resultData != null) {
-                uri = resultData.getData();
+                Uri uri = resultData.getData();
                 filePath = uri.getPath().substring(10);
                 fileUrl.setText(filePath);
-                Log.d("URI",filePath);
+            }
+        } else if (requestCode == QR_CODE_SCANNER_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            if (resultData != null) {
+                String[] addr = resultData.getData().toString().split(":");
+                serverHost = addr[0];
+                serverPort = Integer.parseInt(addr[1]);
+                if(filePath != null) {
+                    Thread cThread = new Thread(new ServerThread());
+                    cThread.start();
+                }
+                else
+                {
+                    Log.d("ERROR","No file picked");
+                }
             }
         }
     }
 
-
+    // OnClick Listeners
     @Override
     public void onClick(View v) {
         int id = v.getId();
-        if(id == R.id.connect_button){
-            if(filePath != null) {
-                Log.d("TAG", "onClick: ok");
-                try {
-                    serverIpAddress = host.getText().toString();
-                    serverPort = Integer.parseInt(port.getText().toString());
-                }catch (NumberFormatException e){
-                    Toast.makeText(MainActivity.this,"Wrong host or port !",Toast.LENGTH_SHORT).show();
-                }
-                Thread cThread = new Thread(new ServerThread());
-                cThread.start();
-            }
-            else
-            {
-                Toast.makeText(MainActivity.this,"No file picked !",Toast.LENGTH_SHORT).show();
-            }
-        }else if (id == R.id.file_picker) {
+        if (id == R.id.file_picker) {
             performFileSearch();
+        } else if (id == R.id.scan_code) {
+            startActivityForResult(new Intent(this, BarcodeScanningActivity.class), QR_CODE_SCANNER_REQUEST_CODE);
         }
     }
 
-
-    public class ServerThread implements Runnable{
+    // File Sending Logic
+    public class ServerThread implements Runnable {
         @Override
         public void run() {
             Socket socket = null;
             try {
-                socket = new Socket(serverIpAddress, serverPort);
+                socket = new Socket(serverHost, serverPort);
             } catch (Exception e) {
                 MainActivity.this.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        Toast.makeText(MainActivity.this,"Connection Failed ! Try Again", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "Connection Failed ! Try Again", Toast.LENGTH_SHORT).show();
                     }
                 });
                 return;
