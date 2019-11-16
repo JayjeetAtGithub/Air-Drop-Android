df1df3454931f31918e483b095a6d545e2c55f5e
Jayjeet Chakraborty <jc.github@rediffmail.com>
2019-11-16 19:04:23 +0530

    Fix file picker feature

--- app/src/main/java/com/pyp2p/jayjeet/pyp2pandroid/MainActivity.java
+++ app/src/main/java/com/pyp2p/jayjeet/pyp2pandroid/MainActivity.java
@@ -23,94 +23,101 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private static final int QR_CODE_SCANNER_REQUEST_CODE = 1;
     private static final int FILE_BROWSER_REQUEST_CODE = 2;
-    Socket socket = null;
-    String serverHost;
-    Integer serverPort;
-    String filePath = null;
-    Button filePicker, scanQrCode, sendFile;
-    TextView fileUrl;
+    private static Socket socket = null;
+    private static String filePath = null;
+
+    private Button filePickerButton, scanButton, sendButton;
+    private TextView fileUrlText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        filePicker = findViewById(R.id.file_picker);
-        scanQrCode = findViewById(R.id.scan_code);
-        sendFile = findViewById(R.id.send_file);
-        fileUrl = findViewById(R.id.file_url);
+        filePickerButton = findViewById(R.id.file_picker_button);
+        scanButton = findViewById(R.id.scan_button);
+        sendButton = findViewById(R.id.send_button);
+        fileUrlText = findViewById(R.id.file_url_text);
 
-        filePicker.setOnClickListener(this);
-        scanQrCode.setOnClickListener(this);
-        sendFile.setOnClickListener(this);
+        filePickerButton.setOnClickListener(this);
+        scanButton.setOnClickListener(this);
+        sendButton.setOnClickListener(this);
     }
 
-
-    // Open the file browser intent
     public void performFileSearch() {
-        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         intent.setType("*/*");
         startActivityForResult(intent, FILE_BROWSER_REQUEST_CODE);
     }
 
-
-    // This function is called after getting result from activity
     @Override
-    public void onActivityResult(int requestCode, int resultCode,
-                                 Intent resultData) {
-
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
         if (requestCode == FILE_BROWSER_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
             if (resultData != null) {
                 Uri uri = resultData.getData();
-                filePath = uri.getPath().substring(10);
-                fileUrl.setText(filePath);
+                FileUtils utils = new FileUtils(getApplicationContext(), uri);
+                String selectedFilePath = utils.getPath();
+                filePath = selectedFilePath;
+                fileUrlText.setText(selectedFilePath);
             }
-        } else if (requestCode == QR_CODE_SCANNER_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+        }
+        else if (requestCode == QR_CODE_SCANNER_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
             if (resultData != null) {
-                String[] addr;
                 try {
-                    addr = resultData.getData().toString().split(":");
-                    serverHost = addr[0];
-                    serverPort = Integer.parseInt(addr[1]);
+                    String[] addr = String.valueOf(resultData.getData()).split(":");
+                    Thread conn_thread = new Thread(new ConnectionRunnable(
+                            String.valueOf(addr[0]),
+                            Integer.parseInt(addr[1])));
+                    conn_thread.start();
                 } catch (Exception e) {
-                    Toast.makeText(getApplicationContext(), "Wrong QR Code was Scanned !", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(
+                            getApplicationContext(), "Wrong QR Code was Scanned !", Toast.LENGTH_SHORT).show();
                 }
-                Thread conn_thread = new Thread(new ConnectionThread());
-                conn_thread.start();
             }
         }
     }
 
-    // OnClick Listeners
     @Override
     public void onClick(View v) {
         int id = v.getId();
-        if (id == R.id.file_picker) {
+        if (id == R.id.file_picker_button) {
             performFileSearch();
-        } else if (id == R.id.scan_code) {
-            startActivityForResult(new Intent(this, BarcodeScanningActivity.class), QR_CODE_SCANNER_REQUEST_CODE);
-        } else if (id == R.id.send_file) {
+        }
+        else if (id == R.id.scan_button) {
+            startActivityForResult(
+                    new Intent(this, BarcodeScanningActivity.class), QR_CODE_SCANNER_REQUEST_CODE);
+        }
+        else if (id == R.id.send_button) {
             if (filePath != null) {
                 Thread send_thread = new Thread(new SendingThread());
                 send_thread.start();
-            } else {
+            }
+            else {
                 Toast.makeText(getApplicationContext(), "No File Picked", Toast.LENGTH_SHORT).show();
             }
         }
     }
 
-    public class ConnectionThread implements Runnable {
+    public class ConnectionRunnable implements Runnable {
+
+        private String serverHost;
+        private Integer serverPort;
+
+        public ConnectionRunnable(String serverHost, Integer serverPort) {
+            this.serverHost = serverHost;
+            this.serverPort = serverPort;
+        }
 
         @Override
         public void run() {
             try {
-                socket = new Socket(serverHost, serverPort);
+                socket = new Socket(this.serverHost, this.serverPort);
                 MainActivity.this.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        sendFile.setEnabled(true);
                         Toast.makeText(getApplicationContext(), "Connection Successfull !", Toast.LENGTH_SHORT).show();
+                        sendButton.setEnabled(true);
                     }
                 });
 
@@ -126,7 +133,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    // Connection Establishment Logic
     public class SendingThread implements Runnable {
         @Override
         public void run() {
@@ -148,20 +154,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 os.write(bytes, 0, bytes.length);
                 os.flush();
                 socket.close();
+
             } catch (IOException e) {
                 e.printStackTrace();
+
             } finally {
                 filePath = null;
+                socket = null;
                 MainActivity.this.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        fileUrl.setText("No File Choosen");
-                        sendFile.setEnabled(false);
+                        fileUrlText.setText(R.string.no_file_choosen);
+                        sendButton.setEnabled(false);
+                        Toast.makeText(getApplicationContext(), "File sent successfully :)", Toast.LENGTH_SHORT).show();
                     }
                 });
             }
         }
     }
 }
-
 
